93d166310d24072f0a70cccb5f01865d37072629
John Hamilton <john.hamilton.developer@gmail.com>
2022-06-01 12:43:06 +1000

    prints out 10 test values in a loop

--- src/practiseJohn/second.java
+++ src/practiseJohn/second.java
@@ -1,4 +1,9 @@
 package practiseJohn;
 
 public class second {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Test values:" + (i+1));
+        }
+    }
 }
