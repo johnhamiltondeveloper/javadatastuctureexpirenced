f66f3b057288e73cfdf539647b45db40bc57533e
John Hamilton <john.hamilton.developer@gmail.com>
2022-06-01 14:59:16 +1000

    trying differnt types of lists

--- src/practise/First.java
+++ src/practise/First.java
@@ -1,7 +1,40 @@
 package practise;
 
+import lists.classes.Employee;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class First {
     public static void main(String args[]){
+
+
+
+        List<String> names = new ArrayList<String>();
+
+        names.add("name");
+        names.add("bob");
+
+        for (int i = 0; i < names.size(); i++) {
+            System.out.println(names.get(i));
+        }
+
+        List<Employee> employeeList = new ArrayList();
+        employeeList.add(new Employee("Jane", "Jones", 123));
+        employeeList.add(new Employee("John", "Doe", 4567));
+        employeeList.add(new Employee("Mary", "Smith", 22));
+        employeeList.add(new Employee("Mike", "Wilson", 3245));
+        employeeList.add(new Employee("Saad", "", 7777));
+        System.out.println(employeeList.get(4));
+
+        List<Integer> temp=new ArrayList<Integer>();
+        temp.add(1);
+        temp.add(2);
+        temp.add(3);
+        System.out.println(temp.size());
+        for(Integer a:temp){
+            System.out.println(a);
+        }
         System.out.println("Hello everyone");
     }
 }
